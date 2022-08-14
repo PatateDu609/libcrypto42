c35d95760aa74df6f56791fc2bc7d10a563a4c8d
Ghali Boucetta <gboucett@student.42.fr>
2022-08-14 02:55:39 +0200

    Adding a few functions to rand.c
    These functions may be used to simplify the use of the random number generator.
    There is functions to generate random strings and random arrays for example.

--- src/common/rand.c
+++ src/common/rand.c
@@ -31,3 +31,48 @@ uint64_t get_random(void)
 	close(fd);
 	return random;
 }
+
+uint8_t *get_random_bytes_at(uint8_t *ptr, uint64_t length)
+{
+	for (uint64_t i = 0; i < length; i++)
+		ptr[i] = get_random() & 0xFF;
+	return ptr;
+}
+
+uint8_t *get_random_bytes(size_t len)
+{
+	uint8_t *bytes = malloc(len);
+	if (!bytes)
+		return NULL;
+	return get_random_bytes_into(bytes, len);
+}
+
+char *get_random_string_at(char *ptr, uint64_t length)
+{
+	for (uint64_t i = 0; i < length; i++)
+	{
+		bool upper = get_random() & 1;
+		ptr[i] = upper ? get_random_range('A', 'Z') : get_random_range('a', 'z');
+	}
+	ptr[length] = '\0';
+	return ptr;
+}
+
+char *get_random_string(size_t len)
+{
+	char *str = malloc(len + 1);
+	if (!str)
+		return NULL;
+	return get_random_string_at(str, len);
+}
+
+char *get_random_string_from(const char *charset, size_t len)
+{
+	char *str = malloc(len + 1);
+	if (!str)
+		return NULL;
+	for (size_t i = 0; i < len; i++)
+		str[i] = charset[get_random_range(0, strlen(charset))];
+	str[len] = '\0';
+	return str;
+}
