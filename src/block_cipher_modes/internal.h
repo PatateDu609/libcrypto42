eace2821e10192159ff8940a8b74fff3aab08ce4
Ghali Boucetta <gboucett@student.42.fr>
2023-07-29 15:50:02 +0200

    Add tests for padding and unpadding using PKCS7

--- src/block_cipher_modes/internal.h
+++ src/block_cipher_modes/internal.h
@@ -1,28 +1,22 @@
-/**
- * @file internal.h
- * @author Ghali Boucetta (gboucett@student.42.fr)
- * @brief Internal functions for block cipher modes.
- * @date 2022-08-15
- *
- * @note This is mainly helper functions to call the effective block cipher functions.
- */
-
 #ifndef BLOCK_CIPHER_MODES_INTERNAL_H
 #define BLOCK_CIPHER_MODES_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "cipher.h"
 #include "common.h"
 #include <stdbool.h>
 
-enum cipher_mode
-{
+enum cipher_mode {
 	CIPHER_MODE_ECB,
 	CIPHER_MODE_CBC,
 };
 
 struct block {
 	uint8_t *data;
-	size_t size;
+	size_t   size;
 };
 
 /**
@@ -34,7 +28,7 @@ struct block {
  *
  * @return Returns true if the context is valid, false otherwise.
  */
-bool __cipher_ctx_valid(struct cipher_ctx *ctx, enum cipher_mode cipher_mode, bool enc) __internal;
+bool     __cipher_ctx_valid(struct cipher_ctx *ctx, enum cipher_mode cipher_mode, bool enc) __internal;
 
 /**
  * @brief Pad the plaintext with the given padding.
@@ -64,10 +58,14 @@ uint8_t *pad(uint8_t *plaintext, size_t *len, size_t blk_size) __internal;
  */
 uint8_t *unpad(uint8_t *plaintext, size_t *len) __internal;
 
-void block_xor(struct block *res, const struct block *a, const struct block *b) __internal;
+void     block_xor(struct block *res, const struct block *a, const struct block *b) __internal;
 
-void block_encrypt(const struct cipher_ctx *ctx, struct block *res, const struct block *a) __internal;
+void     block_encrypt(const struct cipher_ctx *ctx, struct block *res, const struct block *a) __internal;
 
-void block_decrypt(const struct cipher_ctx *ctx, struct block *res, const struct block *a) __internal;
+void     block_decrypt(const struct cipher_ctx *ctx, struct block *res, const struct block *a) __internal;
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
